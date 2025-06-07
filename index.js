cb05e7a89cdb5e3274b1d41afaa6ed76e30475b6
unknown <nickoluoch1130@gmail.com>
2025-06-07 20:49:00 +0300

    code

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function stockList(books, categories) {
-    return '';
+   const total = {};
+
+   books.forEach(item => {
+    const [code,qty] = item.split(' ');
+    const letter = code[0];
+    const amount = Number(qty);
+
+    if (categories.includes(letter)) {
+        total[letter] = (total[letter] || 0) +amount;
+    }
+   });
+
+   
+
+
+ 
 }
+
+console.log(stockList(  ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"],
+            ["A", "B", "C", "D"]));
